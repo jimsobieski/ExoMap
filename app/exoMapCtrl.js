ba8bd20b2413e498dcc926a6395f11d1f878ac09
Sébastien Taton <jimsobieski21@gmail.com>
2018-02-06 18:50:09 +0100

    sidenav toggle

--- app/exoMapCtrl.js
+++ app/exoMapCtrl.js
@@ -1,3 +1,21 @@
-angular.module('myApp.ExoMapCtrl', ['ngRoute']).controller('ExoMapCtrl', function() {
+angular.module('myApp.ExoMapCtrl', ['ngRoute']).controller('ExoMapCtrl', function($scope, $mdSidenav, $window) {
+
+    $scope.sizeMap = 100;
+
+    $scope.toggleSidenav = function() {
+        $mdSidenav('right').toggle();
+        changeSizeMap();
+    };
+
+    var changeSizeMap = function () {
+        console.log($window.innerWidth);
+        console.log($mdSidenav);
+        if($scope.sizeMap === 100) {
+            $scope.sizeMap = 80;
+        }
+        else {
+            $scope.sizeMap = 100;
+        }
+    }
 
 });
